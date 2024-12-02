092bb8a94df302e5264dfe31a67aba1d6699b458
Etishree Behera <etishreebehera281@gmail.com>
2024-12-02 13:13:44 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,12 @@
 provider "aws"{
 region = "ap-south-1"
 }
-
-resource "aws_instance"  "AWSInstance" {
+resource "aws_instance" "AWSInstance" {
    ami = "ami-0614680123427b75e"
    instance_type = "t2.micro"
-   key_name = "devopsnov2024"
+   key_name = "devopsaug2024"
    security_groups = ["launch-wizard-2"]
    tags={
-   Name = "terraformserver"
-   }
+   name = "terraformserver"
+  }
 }
